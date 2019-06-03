e611bfb7e9f50a757808b351be069fde0ec655cc
Von Ralls <vonralls@gmail.com>
2019-06-03 11:59:09 -0500

    Commit after setting up basic views.

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title')
+    Welcome to vonralls.com
+@endsection
+
+@section('content')
+    <h1>This is a website about my technological experiences. Ya dig?
+@endsection
