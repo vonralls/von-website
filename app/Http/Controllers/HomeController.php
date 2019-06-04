2e5c20dd87dffdb528eb511588416f4a28a6bfbc
Von Ralls <vonralls@gmail.com>
2019-06-04 09:44:09 -0500

    cahnges

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
 
 class HomeController extends Controller
@@ -26,7 +27,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $posts = DB::table('users')->leftjoin('posts', 'users.id', '=', 'posts.author')->paginate(10);
+        return view('home', ['posts' => $posts]);
     }
 
     public function getPostForm() {
